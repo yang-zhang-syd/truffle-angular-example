3b2ed98861667f7000d4e28272d2c2e8983291e7
Yang Zhang <yang.zhang@icloud.com>
2018-04-03 06:30:28 +1000

    create account, check balances, transfer fund

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import * as _ from 'lodash';
+import * as fromRoot from 'app/datastore/root-reducers';
+import * as AccountActions from 'app/datastore/account/account-actions';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  protected ngUnsubscribe = new Subject();
+
+  publicAddress: string;
+  publicAddresses: string[];
+  balancesObservable: Observable<Map<string, number>>;
+  balances: any[] = [];
+  interval: any;
+
+  // create new account
+  newPassword: string = '';
+  repeatNewPassword: string = '';
+  newAccountAddress: Observable<string>;
+
+  // transfer fund
+  fromAddr: string;
+  toAddr: string;
+  password: string;
+  amount: string;
+  receipt: Observable<any>;
+
+  constructor(private store: Store<fromRoot.State>) {
+    this.balancesObservable = store.select(state => state.account.balances);
+    this.newAccountAddress = store.select(state => state.account.newAccountAddress);
+    this.receipt = store.select(state => state.account.transferReceipt);
+  }
+
+  ngOnInit() {
+
+    this.balancesObservable.takeUntil(this.ngUnsubscribe).subscribe(accbalances => {
+      var keys = _.keys(accbalances);
+      this.balances = [];
+      _.map(keys, key => {
+        this.balances.push({publicAddress: key, balance: accbalances[key]});
+      });
+    });
+
+    this.store.select(state => state.account.publicAddresses)
+      .takeUntil(this.ngUnsubscribe).subscribe(addresses => {
+        this.publicAddresses = addresses;
+      });
+
+    this.interval = setInterval(() => {
+      if(this.publicAddresses.length != 0) {
+        this.store.dispatch(new AccountActions.GetAccountBalances(this.publicAddresses));
+      }
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+    clearInterval(this.interval);
+  }
+
+  addAccount() {
+    this.store.dispatch(new AccountActions.AddAccount(this.publicAddress));
+  }
+
+  etherAccCloseClicked(address) {
+    this.store.dispatch(new AccountActions.RemoveAccount(address));
+  }
+
+  createAccount() {
+    if(this.newPassword === this.repeatNewPassword) {
+      this.store.dispatch(new AccountActions.CreateAccount(this.newPassword));
+    }
+  }
+
+  transferEther() {
+    this.store.dispatch(new AccountActions.TransferEther({
+      fromAddr: this.fromAddr,
+      toAddr: this.toAddr,
+      amount: this.amount,
+      password: this.password
+    }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { EtherAccountComponent } from './components/ether.account.component';
+import { DataStoreModule } from 'app/datastore/datastore.module';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EtherAccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    DataStoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
