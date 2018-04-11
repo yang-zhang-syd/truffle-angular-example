e62402a22924a8a4da83767d4511b259df1ea872
Yang Zhang <yang.zhang@icloud.com>
2018-04-11 22:32:20 +1000

    cleaned up page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,38 +22,23 @@ export class AppComponent {
   private EthereumNodeUrl = environment.ethereumNodeUrl;
   protected ngUnsubscribe = new Subject();
 
-  publicAddress: string;
-  publicAddresses: string[];
-  balancesObservable: Observable<Map<string, number>>;
-  balances: any[] = [];
+  accountAddr: string;
+  fromAddr: string;
+  toAddr: string;
+  amount: number;
+  balancesMap = {};
+  accounts = [];
+  receipt: any;
   interval: any;
-
-  // create new account
-  newAccountAddress: Observable<string>;
-  newAccountForm: FormGroup;
-
-  // transfer fund
-  receipt: Observable<any>;
   transferFundForm: FormGroup;
 
   MetaCoin = contract(artifacts);
 
   constructor(private store: Store<fromRoot.State>, private fb: FormBuilder) {
-    this.balancesObservable = store.select(state => state.account.balances);
-    this.newAccountAddress = store.select(state => state.account.newAccountAddress);
-    this.receipt = store.select(state => state.account.transferReceipt);
-
-    this.newAccountForm = this.fb.group({
-        newPassword: ['', [Validators.required, Validators.minLength(6)]],
-        repeatNewPassword: ['', [Validators.required, Validators.minLength(6)]],
-      }, 
-      {validator: this.passwordMatchValidator}
-    );
 
     this.transferFundForm = this.fb.group({
       fromAddr: ['', [Validators.required, Validators.minLength(40)]],
       toAddr: ['', [Validators.required, Validators.minLength(40)]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
       amount: ['', [Validators.required]]
     });
 
@@ -62,31 +47,13 @@ export class AppComponent {
     this.MetaCoin.setProvider(provider);
   }
 
-  passwordMatchValidator(frm: FormGroup) {
-    return frm.controls['newPassword'].value === frm.controls['repeatNewPassword'].value
-       ? null : {'mismatch': true};
-  }
-
   ngOnInit() {
-
-    this.balancesObservable.takeUntil(this.ngUnsubscribe).subscribe(accbalances => {
-      var keys = _.keys(accbalances);
-      this.balances = [];
-      _.map(keys, key => {
-        this.balances.push({publicAddress: key, balance: accbalances[key]});
-      });
-    });
-
-    this.store.select(state => state.account.publicAddresses)
-      .takeUntil(this.ngUnsubscribe).subscribe(addresses => {
-        this.publicAddresses = addresses;
-      });
-
     this.interval = setInterval(() => {
-      if(this.publicAddresses.length != 0) {
-        this.store.dispatch(new AccountActions.GetAccountBalances(this.publicAddresses));
-      }
-    }, 5000);
+      _.map(this.accounts, acc => {
+        console.log('check balance for ' + acc);
+        this.checkBalanceWithAddress(acc);
+      });
+    }, 2000);
   }
 
   ngOnDestroy() {
@@ -95,44 +62,31 @@ export class AppComponent {
     clearInterval(this.interval);
   }
 
-  testContract() {
-    this.MetaCoin.deployed().then((instance) => {
-      //console.log(instance);
-      instance.getBalance('0x5e46E398294606658DC62B46E3Cc9e85aE7B725c')
-        .then((balance) => {
-            console.log(balance);
-        });
+  checkBalance() {
+    this.checkBalanceWithAddress(this.accountAddr);
+  }
 
-      instance.sendCoin('0xd614E92B5d0E2a67deDBbdF416b5a3dAd5f78604', 1, {from: '0x5e46E398294606658DC62B46E3Cc9e85aE7B725c'})
-        .then(result => {
-          console.log(result);
+  checkBalanceWithAddress(addr: string) {
+    this.MetaCoin.deployed().then((instance) => {
+      instance.getBalance(addr)
+        .then((balance) => {
+            this.balancesMap[addr] = balance.toString();
+            this.accounts = _.keys(this.balancesMap);
         });
     });
   }
 
-  addAccount() {
-    this.store.dispatch(new AccountActions.AddAccount(this.publicAddress));
+  sendCoin() {
+    this.MetaCoin.deployed().then((instance) => {
+      instance.sendCoin(this.transferFundForm.value.toAddr, this.transferFundForm.value.amount, {from: this.transferFundForm.value.fromAddr})
+        .then(result => {
+          this.receipt = result;
+        });
+    });
   }
 
-  etherAccCloseClicked(address) {
-    this.store.dispatch(new AccountActions.RemoveAccount(address));
-  }
-
-  createAccount() {
-    if(!this.newAccountForm.errors) {
-      this.store.dispatch(new AccountActions.CreateAccount(this.newAccountForm.value.newPassword));
-    }
-  }
-
-  transferEther() {
-    if(this.transferFundForm.errors)
-      return;
-
-    this.store.dispatch(new AccountActions.TransferEther({
-      fromAddr: this.transferFundForm.value.fromAddr,
-      toAddr: this.transferFundForm.value.toAddr,
-      amount: this.transferFundForm.value.amount,
-      password: this.transferFundForm.value.password
-    }));
+  accCloseClicked(addr: string) {
+    this.balancesMap = _.omit(this.balancesMap, [addr]);
+    this.accounts = _.keys(this.balancesMap);
   }
 }
