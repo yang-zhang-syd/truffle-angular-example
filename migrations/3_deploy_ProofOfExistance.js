7206e3b2919f0ec8562641f0e4ca4d05e8e7a517
Yang Zhang <yang.zhang@icloud.com>
2018-04-11 22:34:05 +1000

    removed unrelated contracts

--- migrations/3_deploy_ProofOfExistance.js
+++
@@ -1,8 +0,0 @@
-var ProofOfExistence = artifacts.require("./ProofOfExistence.sol");
-var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
-var ProofOfExistence3 = artifacts.require("./ProofOfExistence3.sol");
-module.exports = function(deployer) {
-  deployer.deploy(ProofOfExistence);
-  deployer.deploy(ProofOfExistence2);
-  deployer.deploy(ProofOfExistence3);
-};
